7fdd5ec78a0a69084ffdd12c11a7981dfda20f93
ידידיה קורן <ykorn@g.jct.ac.il>
2019-11-11 11:57:25 +0200

    tyota

--- HW1/tree.cpp
+++ HW1/tree.cpp
@@ -3,29 +3,83 @@
 DecisionTreeNode::~DecisionTreeNode()
 {	
 	if (!answers.empty())
-		answers.front()->son->~DecisionTreeNode;
+	{
+		for_each(answers.begin(),answers.end(),answers.front()->son->~DecisionTreeNode); 
+	}
 	answers.~list();
 }
 
-DecisionTreeNode * DecisionTreeNode::search(string str, DecisionTreeNode*& father=nullptr)
+void DecisionTreeNode::addAnswer(string str,string son)
 {
+	Answer* temp = new Answer(str);
+	answers.push_back(temp);
+	DecisionTreeNode* temp2 = new DecisionTreeNode(son);
+	temp->setSon(temp2);
+	temp2->isLeaf = true;
+}
+
+DecisionTreeNode * DecisionTreeNode::search(string str, DecisionTreeNode*& father)
+{
+
 	if (this->_value == str)
 		return this;
 	if (this->isLeaf == true)
 		return ;
-	for (auto it =this->answers.begin();it!=this->answers.end();it++)
+	for_each(this->answers.begin(), this->answers.end(), search(str,father));
+	
+}
+
+void DecisionTreeNode::ptint()
+{
+	cout << _value << endl;
+	for_each(answers.begin(), answers.end(), [](Answer & ans) {cout<<ans.ans<<endl<<ans.son.print(); });
+}
+
+bool DecisionTreeNode::show()
+{
+	if (!isLeaf())
 	{
-		return this->search(str);
+		DecisionTreeNode* tmp;
+		string temp;
+		cout << _value << endl;
+		cin >> temp;
+		tmp = tmp->search(temp,tmp);
+		tmp->show();
 	}
+	return false;
 }
 
 Tree::~Tree()
 {
-	root.~DecisionTreeNode();
+	_root->~DecisionTreeNode();
 }
 
 DecisionTreeNode * Tree::search(string str ,DecisionTreeNode*& father)
 {
-	_root->search(str, , DecisionTreeNode* father)
+	_root->search(str,father);
 	return nullptr;
+
+}
+
+bool Tree::addSon(string father, string val, string sol)
+{
+	DecisionTreeNode* temp;
+	if (temp=nullptr)
+		return false;
+	else
+	{
+		if (temp->isLeaf())
+			temp->isLeaf(false);
+		temp->addAnswer(val,sol);
+	}
+}
+
+void Tree::printAllTree()
+{
+	_root->ptint();
+}
+
+void Tree::process()
+{
+	_root.show();
 }
--- HW1/tree.h
+++ HW1/tree.h
@@ -11,27 +11,35 @@ class DecisionTreeNode
 {
 public:
 	DecisionTreeNode();
+	DecisionTreeNode(string val) { _value = val; }
 	~DecisionTreeNode();
+	void addAnswer(string, string);
+	bool isLeaf() { return _isLeaf; }
+	bool isLeaf(bool b) { _isLeaf=b; }
+	void setValue(string str) { _value = str; }
+	DecisionTreeNode* search(string str ,DecisionTreeNode*& father);
+	void ptint();
+	bool show();
+
 protected:
 	string _value;
 	list<Answer*> answers;
-	bool isLeaf;
+	bool _isLeaf;
 
-public:
-	//DecisionTreeNode& operator=(const DecisionTreeNode&);
-	//DecisionTreeNode& operator=(DecisionTreeNode&&);
-
-	void setValue(string str) { _value = str; }
-	DecisionTreeNode* search(string str ,DecisionTreeNode*& father);
 };
 
 class Answer
 {
 public:
 	Answer();
-	~Answer();
-	string ans;
-	DecisionTreeNode* son;
+	Answer(string ans) { _ans = ans; }
+	~Answer() { delete _son; }
+	void setSon(DecisionTreeNode* son) { _son = son; }
+	DecisionTreeNode* son() { return _son; }
+	string ans() { return _ans; }
+protected:
+	string _ans;
+	DecisionTreeNode* _son;
 };
 class Tree: public DecisionTreeNode
 {
@@ -45,5 +53,10 @@ public:
 	~Tree();
 	void addRoot(string str) { _root->setValue(str); }
 	DecisionTreeNode* search(string str, DecisionTreeNode*& father);
+	bool addSon(string father, string val, string sol);
+	void printAllTree();
+	void searchAndPrint(string val) { DecisionTreeNode* temp; temp = temp->search(val, _root); temp->ptint(); delete temp; }
+	void deleteSubTree(string val) { DecisionTreeNode* temp; temp = temp->search(val, _root); temp->~DecisionTreeNode; delete temp; }
+	void process();
 };
 
